e1a6f077a85b6f90cd710e1ae321b46dc833cb0a
thorgnir <thorgnir@gmail.com>
2014-11-09 17:14:20 +0300

    phpmailer

---
+++ src/php/smtp_config.template.php
@@ -0,0 +1,12 @@
+<?php
+$__smtp = array(
+    "host" => "smtp.gmail.com",						//smtp server
+    "debug" => 2,                   				//debug level
+    "auth" => true,                					//auth YES
+    "port" => 465,                    				//secure smtp port
+    "username" => "",						//username on the server
+    "password" => "",						//email password
+    "addreply" => "",					//full email
+    "replyto" => ""					//reply-to email
+);
+?>
