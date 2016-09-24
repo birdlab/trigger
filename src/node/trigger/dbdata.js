7b8919c17c1d622a198c85d08a6663afefb5e890
birdlab <mail@birdlab.ru>
2016-09-24 16:49:21 +0300

    top sort fix

--- src/node/trigger/dbdata.js
+++ src/node/trigger/dbdata.js
@@ -359,18 +359,16 @@ exports.getTracksByShift = function(data, callback) {
         gettitle = ' AND tracks.title LIKE "%' + data.title + '%"';
     }
     var q = '';
-    var date='playdate';
-    if (data.gold){
-        date='date'
-    }
+    var date = 'playdate';
     if (!data.top) {
-        q = 'SELECT tracks.*, users.name FROM tracks LEFT JOIN users ON tracks.submiter=users.id WHERE tracks.channel=' + data.channel + ' AND tracks.'+date+' < ' + db.connection.escape(data.shift) + getgold + getartist + gettitle + ' order by tracks.playdate desc LIMIT 20';
+        q = 'SELECT tracks.*, users.name FROM tracks LEFT JOIN users ON tracks.submiter=users.id WHERE tracks.channel=' + data.channel + ' AND tracks.' + date + ' < ' + db.connection.escape(data.shift) + getgold + getartist + gettitle + ' order by tracks.playdate desc LIMIT 20';
     } else {
-        var order = ' ORDER BY tracks.rating DESC, tracks.realrating DESC, tracks.playdate DESC ';
+        date = 'date'
+        var order = ' ORDER BY tracks.rating DESC, tracks.realrating DESC, tracks.' + date + '  DESC ';
         if (data.votes) {
-            order = ' ORDER BY tracks.realrating DESC, tracks.rating DESC, tracks.playdate DESC ';
+            order = ' ORDER BY tracks.realrating DESC, tracks.rating DESC, tracks.' + date + ' DESC ';
         }
-        q = 'SELECT tracks.*, users.name FROM tracks LEFT JOIN users ON tracks.submiter=users.id WHERE tracks.channel=' + data.channel + ' AND tracks.'+date+' BETWEEN NOW() - INTERVAL 7 DAY AND NOW()' + getgold + getartist + gettitle + order + 'LIMIT ' + data.shift + ',20'
+        q = 'SELECT tracks.*, users.name FROM tracks LEFT JOIN users ON tracks.submiter=users.id WHERE tracks.channel=' + data.channel + ' AND tracks.playdate BETWEEN NOW() - INTERVAL 7 DAY AND NOW()' + getgold + getartist + gettitle + order + 'LIMIT ' + data.shift + ',20'
     }
     var finded = false;
     for (var i in historyStack) {
@@ -793,9 +791,9 @@ exports.getTrackVotes = function(trackid, callback) {
     db.connection.query(q, function(error, result, fields) {
         if (!error) {
             var votes = {
-                positive:[],
-                negative:[],
-                rating:0
+                positive: [],
+                negative: [],
+                rating: 0
             };
             for (var v in result) {
                 var vote = {
